707bb4e54b9d0fc21f71618daec37a81600e1a14
Sebastian Lindmark <sebbe.lindmark@gmail.com>
2020-04-09 12:28:52 +0200

    Add async product fetch

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet,Text } from 'react-native';
-import { createStore, combineReducers} from 'redux'
+import { createStore, combineReducers, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 
 import productsReducer from './store/reducers/products'
@@ -8,9 +8,12 @@ import cartReducer from './store/reducers/cart'
 import orderReducer from './store/reducers/orders'
 import ShopNavigator from './navigation/ShopNavigator'
 
+import ReduxThunk from 'redux-thunk'
+
+
 const rootReducer = combineReducers({products : productsReducer,cart : cartReducer, orders : orderReducer})
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk))
 
 export default function App() {
   return (
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,27 +1,49 @@
-import React from 'react';
-import { FlatList, Text, Platform, Button } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { FlatList, Text, Platform, Button, ActivityIndicator, View, StyleSheet} from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import ProductItem from '../../components/shop/ProductItem'
 import * as cartActions from '../../store/actions/cart'
-import { HeaderButtons, Item } from 'react-navigation-header-buttons'
-import HeaderButton from '../../components/UI/HeaderButton'
-import Colors from '../../constants/Colors'
-
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../../components/UI/HeaderButton';
+import Colors from '../../constants/Colors';
+import * as productsActions from '../../store/actions/products';
 
 const ProductsOverviewScreen = (props) => {
 
     //returns the redux state
     const products = useSelector(state => state.products.availableProducts);
-    const dispath = useDispatch();
+    const dispatch = useDispatch();
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+
+        const loadProducts = async () => {
+
+            setIsLoading(true);
+            await dispatch(productsActions.fetchProducts());
+            setIsLoading(false);
+        }
+
+        loadProducts();
+
+
+    }, [dispatch]);
 
     const selectItemHandler = (id, title) => {
         props.navigation.navigate('ProductDetail', { productId: id, productTitle: title })
     }
 
+    if (isLoading) {
+        return <View style={styles.centered}>
+            <ActivityIndicator size='large' color={Colors.primary} />
+        </View>
+    }
+
     return <FlatList
         data={products}
         keyExtractor={item => item.id}
-        renderItem={itemData => 
+        renderItem={itemData =>
             <ProductItem
                 image={itemData.item.imageUrl}
                 title={itemData.item.title}
@@ -29,7 +51,7 @@ const ProductsOverviewScreen = (props) => {
                 onSelect={() => selectItemHandler(itemData.item.id, itemData.item.title)}>
 
                 <Button color={Colors.primary} title="View Details" onPress={() => selectItemHandler(itemData.item.id, itemData.item.title)} />
-                <Button color={Colors.primary} title="To Cart" onPress={() => dispath(cartActions.addToCart(itemData.item))} />
+                <Button color={Colors.primary} title="To Cart" onPress={() => dispatch(cartActions.addToCart(itemData.item))} />
 
             </ProductItem>
         } />
@@ -52,5 +74,11 @@ ProductsOverviewScreen.navigationOptions = navData => {
     }
 
 }
+
+
+const styles = StyleSheet.create({
+
+    centered: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+})
 
 export default ProductsOverviewScreen;
